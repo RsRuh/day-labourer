6240a6e0802a3acfcae9a5dec74816ba8081b37f
RsRuh <rshridoysr@gmail.com>
2022-11-10 05:14:24 -0600

    add second question and ans of blog section

--- src/Component/Pages/Blog.js
+++ src/Component/Pages/Blog.js
@@ -54,7 +54,37 @@ const Blog = () => {
                                 SQL is the programming language used to interface with relational databases. (Relational databases model data as records in rows and tables with logical links between them). NoSQL is a class of DBMs that are non - relational and generally do not use SQL.
                             </p >
                         </details >
+                        <details className="group border-l-4 border-[#3F0071] bg-gray-50 p-6" >
+                            <summary className="flex cursor-pointer items-center justify-between" >
+                                <h2 className="text-lg font-medium text-gray-900" >
+                                    What is JWT, and how does it work ?
+                                </h2 >
 
+                                <span
+                                    className="ml-1.5 flex-shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3"
+                                >
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="h-5 w-5 flex-shrink-0 transition duration-300 group-open:-rotate-45"
+                                        viewBox="0 0 20 20"
+                                        fill="currentColor"
+                                    >
+                                        <path
+                                            fillRule="evenodd"
+                                            d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                                            clipRule="evenodd"
+                                        />
+                                    </svg >
+                                </span >
+                            </summary >
+
+                            <p className="mt-4 leading-relaxed text-gray-700" >
+                                JSON Web Token (JWT) is an open standard (RFC 7519) that defines a compact and self-contained way for securely transmitting information between parties as a JSON object. This information can be verified and trusted because it is digitally signed. JWTs can be signed using a secret (with the HMAC algorithm) or a public/private key pair using RSA or ECDSA.
+                                <br />
+                                <br />
+                                Authentication server verifies the credentials and issues a jwt signed using either a secret salt or a private key. User's Client uses the JWT to access protected resources by passing the JWT in HTTP Authorization header. Resource server then verifies the authenticity of the token using the secret salt/ public key
+                            </p >
+                        </details >
                     
                     </div >
                 )
